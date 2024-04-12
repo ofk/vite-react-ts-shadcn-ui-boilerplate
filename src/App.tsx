a27a2396b15d4e213a83c31e71566d53479c3c66
ofk <ofkjpn+github@gmail.com>
2024-04-13 02:09:02 +0900

    Rewrite by tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import reactLogo from '@/assets/react.svg';
 
 // eslint-disable-next-line import/no-absolute-path
 import viteLogo from '/vite.svg';
-import '@/App.css';
 
 export function App(): React.ReactElement {
   const [count, setCount] = useState(0);
@@ -13,27 +12,36 @@ export function App(): React.ReactElement {
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img
+            src={viteLogo}
+            className="hover:drop-shadow-vite-logo inline-block h-24 p-6 transition"
+            alt="Vite logo"
+          />
         </a>
         <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img
+            src={reactLogo}
+            className="hover:drop-shadow-react-logo animate-spin-react-logo inline-block h-24 p-6 transition"
+            alt="React logo"
+          />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
+      <h1 className="text-5xl font-bold">Vite + React</h1>
+      <div className="p-8">
         <button
           type="button"
+          className="cursor-pointer rounded-lg border border-solid border-transparent bg-muted px-5 py-2.5 font-medium transition duration-200 hover:border-indigo-500"
           onClick={() => {
             setCount((prevCount) => prevCount + 1);
           }}
         >
           count is {count}
         </button>
-        <p>
+        <p className="my-4">
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <p className="my-4 text-muted-foreground">Click on the Vite and React logos to learn more</p>
     </>
   );
 }
