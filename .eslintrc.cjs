488a20e4de3cccb1c9d63639a444e2862aa100f2
ofk <ofkjpn+github@gmail.com>
2024-04-13 02:03:08 +0900

    `npx shadcn-ui@latest init`

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,7 +2,14 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: '@ofk/eslint-config-recommend',
-  ignorePatterns: ['dist', '.eslintrc.cjs', '*.config.cjs', '*.config.ts'],
+  ignorePatterns: [
+    'dist',
+    'src/lib',
+    'src/components/ui',
+    '.eslintrc.cjs',
+    '*.config.cjs',
+    '*.config.ts',
+  ],
   parserOptions: {
     project: './tsconfig.json',
   },
